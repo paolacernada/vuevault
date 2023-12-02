1d1f254db2ac720a26f1d12501a9277af7e1c79a
Paola Cernada <96987381+paolacernada@users.noreply.github.com>
2023-12-02 17:57:50 -0500

    Styled Home.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,21 +1,21 @@
 <template>
     <header class="bg-white shadow h-16 flex justify-between items-center">
-        <router-link :to="{ name: 'home' }" class="inline-flex items-center h-full px-5">
+        <router-link :to="{ name: 'home' }" class="inline-flex items-center h-full px-5 hover:text-blue-500">
             Home
         </router-link>
         <div class="flex-items-center gap-1">
             <router-link :to="{ name: 'byTitle' }"
-                class="inline-flex items-center px-2 h-full transition-colors hover:bg-blue-500 hover:text-white">
+                class="inline-flex items-center px-2 rounded-xl h-full transition-colors hover:bg-blue-500 hover:text-white">
                 Search Movies
             </router-link>
 
             <router-link :to="{ name: 'byLetter' }"
-                class="inline-flex items-center px-2 h-full transition-colors hover:bg-blue-500 hover:text-white">
+                class="inline-flex items-center px-2 rounded-xl h-full transition-colors hover:bg-blue-500 hover:text-white">
                 Featured Movies
             </router-link>
 
             <router-link :to="{ name: 'byCast' }"
-                class="inline-flex items-center px-2 h-full transition-colors hover:bg-blue-500 hover:text-white">
+                class="inline-flex items-center px-2 rounded-xl h-full transition-colors hover:bg-blue-500 hover:text-white">
                 Movies By Cast
             </router-link>
         </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,75 @@
 <template>
-    <div class="flex flex-col p-8">
+    <div class="flex flex-col p-8 text-blue-500">
+        <h1 class="text-4xl font-bold mb-4">Movies now playing in theatres</h1>
+    </div>
 
-        <!-- Alphabet Navigation -->
-        <AlphabetNav />
+    <!-- Display Movies -->
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <MovieCard v-for="movie in moviesNowPlaying" :key="movie.id" :movie="movie" />
+    </div>
 
+    <div class="flex justify-center mt-6">
+        <!-- Load More Button -->
+        <button v-if="totalPages > currentPage" @click="loadMoreMovies"
+            class="w-1/4 px-4 py-2 bg-blue-500 text-white rounded-lg focus:outline-none hover:bg-blue-600 transition-all">
+            Load More
+        </button>
     </div>
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from "vue";
-import store from "../store";
+import { onMounted, ref } from "vue";
 import axiosClient from "../axiosClient.js";
-import AlphabetNav from "../components/AlphabetNav.vue";
+import MovieCard from '../components/MovieCard.vue';
 
-
-const movieCast = ref([])
+const moviesNowPlaying = ref([]);
+const currentPage = ref(1);
+const totalPages = ref(0);
 
 onMounted(async () => {
-    const response = await axiosClient.get("search/movie?query=")
-    console.log(response.json)
-    movieCast.value = response.data
-})
+    await fetchNowPlayingMovies();
+});
 
+async function fetchNowPlayingMovies() {
+    try {
+        const response = await axiosClient.get("/movie/now_playing", {
+            params: { page: currentPage.value }
+        });
+        const movies = response.data.results;
+        const processedMovies = await processMovies(movies);
+        moviesNowPlaying.value = [...moviesNowPlaying.value, ...processedMovies];
+
+        totalPages.value = response.data.total_pages;
+    } catch (error) {
+        console.error("Error fetching now playing movies:", error);
+    }
+}
+
+async function processMovies(movies) {
+    return await Promise.all(movies.map(async (movie) => {
+        return {
+            ...movie,
+            trailerAvailable: await checkTrailerAvailability(movie.id)
+        };
+    }));
+}
+
+async function checkTrailerAvailability(movieId) {
+    try {
+        const response = await axiosClient.get(`/movie/${movieId}/videos`);
+        const videos = response.data.results;
+        const trailerVideo = videos.find(video => video.type === 'Trailer' && video.site === 'YouTube');
+        return !!trailerVideo;
+    } catch (error) {
+        console.error('Error checking trailer availability:', error);
+        return false;
+    }
+}
+
+function loadMoreMovies() {
+    if (currentPage.value < totalPages.value) {
+        currentPage.value++;
+        fetchNowPlayingMovies();
+    }
+}
 </script>
